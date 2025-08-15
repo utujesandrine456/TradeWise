3b33f4f85ec19f4557fa46d8d138e77415212e28
utujesandrine456 <utujesandrine456@gmail.com>
2025-08-15 16:51:05 +0200

    Add: src/backend-form1/routes/sales.js

--- src/backend-form1/routes/sales.js
+++ src/backend-form1/routes/sales.js
@@ -10,25 +10,19 @@ const {
   getSaleStats
 } = require('../controllers/saleController');
 
-// Get all sales
 router.get('/', getAllSales);
 
-// Get sale statistics
 router.get('/stats', getSaleStats);
 
-// Search sales
 router.get('/search', searchSales);
 
-// Get single sale
 router.get('/:id', getSaleById);
 
-// Create new sale
 router.post('/', createSale);
 
-// Update sale
 router.put('/:id', updateSale);
 
-// Delete sale
 router.delete('/:id', deleteSale);
+
 
 module.exports = router;
