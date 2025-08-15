7c031cda5d50bb49a097708c5c8e70bff741e136
utujesandrine456 <utujesandrine456@gmail.com>
2025-08-15 16:51:05 +0200

    Add: src/backend-form1/routes/products.js

--- src/backend-form1/routes/products.js
+++ src/backend-form1/routes/products.js
@@ -10,25 +10,19 @@ const {
   getProductStats
 } = require('../controllers/productController');
 
-// Get all products
+
 router.get('/', getAllProducts);
 
-// Get product statistics
 router.get('/stats', getProductStats);
 
-// Search products
 router.get('/search', searchProducts);
 
-// Get single product
 router.get('/:id', getProductById);
 
-// Create new product
 router.post('/', createProduct);
 
-// Update product
 router.put('/:id', updateProduct);
 
-// Delete product
 router.delete('/:id', deleteProduct);
 
 module.exports = router;
