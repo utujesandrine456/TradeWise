0d59a2b462deee26ec057ddc580a01282f7f4b85
utujesandrine456 <utujesandrine456@gmail.com>
2025-08-15 16:51:05 +0200

    Add: src/backend-form1/routes/purchases.js

--- src/backend-form1/routes/purchases.js
+++ src/backend-form1/routes/purchases.js
@@ -10,25 +10,18 @@ const {
   getPurchaseStats
 } = require('../controllers/purchaseController');
 
-// Get all purchases
 router.get('/', getAllPurchases);
 
-// Get purchase statistics
 router.get('/stats', getPurchaseStats);
 
-// Search purchases
 router.get('/search', searchPurchases);
 
-// Get single purchase
 router.get('/:id', getPurchaseById);
 
-// Create new purchase
 router.post('/', createPurchase);
 
-// Update purchase
 router.put('/:id', updatePurchase);
 
-// Delete purchase
 router.delete('/:id', deletePurchase);
 
 module.exports = router;
