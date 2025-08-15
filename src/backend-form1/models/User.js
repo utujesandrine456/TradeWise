2c44eb3f497bd0048eb81d7e6f31b33f0f909f64
utujesandrine456 <utujesandrine456@gmail.com>
2025-08-15 16:51:04 +0200

    Add: src/backend-form1/models/User.js

--- src/backend-form1/models/User.js
+++ src/backend-form1/models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const { modelName } = require('./Transaction');
 
 
 const userSchema  = new mongoose.Schema({
-    fullName: {type: String, required: true},
-    email: { type: String, required: true, unique: true},
+    CompanyName: {type: String, required: true},
+    businessemail: { type: String, required: true, unique: true},
     password: {type: String, required: true},
     role: {type: String, enum: ['user', 'admin'], default: 'user'},
     isVerified: {type: Boolean, default: false},
