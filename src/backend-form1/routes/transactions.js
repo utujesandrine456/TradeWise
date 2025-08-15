a804ea6f56305c06b134a71a6308a6abf5bc2d8f
utujesandrine456 <utujesandrine456@gmail.com>
2025-08-15 16:51:05 +0200

    Add: src/backend-form1/routes/transactions.js

--- src/backend-form1/routes/transactions.js
+++ src/backend-form1/routes/transactions.js
@@ -10,25 +10,20 @@ const {
   getTransactionStats
 } = require('../controllers/transactionController');
 
-// Get all transactions
+
 router.get('/', getAllTransactions);
 
-// Get transaction statistics
 router.get('/stats', getTransactionStats);
 
-// Search transactions
 router.get('/search', searchTransactions);
 
-// Get single transaction
 router.get('/:id', getTransactionById);
 
-// Create new transaction
 router.post('/', createTransaction);
 
-// Update transaction
 router.put('/:id', updateTransaction);
 
-// Delete transaction
 router.delete('/:id', deleteTransaction);
+
 
 module.exports = router;
