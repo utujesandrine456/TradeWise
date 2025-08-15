828d166b1b1725b2620a3eb060b2f9537c3fceb1
utujesandrine456 <utujesandrine456@gmail.com>
2025-08-15 16:51:06 +0200

    Add: src/components1/DashboardLayout.jsx

--- src/components1/DashboardLayout.jsx
+++ src/components1/DashboardLayout.jsx
@@ -13,7 +13,7 @@ import SellingProducts from './SellingProducts';
 import Notification from './Notification';
 import CreditsDebit from './CreditsDebit';
 import ShoppingCart from './ShoppingCart';
-
+import '../index.css'
 
 const DashboardLayout = () => {
   const [activeTab, setActiveTab] = useState('dashboard');
@@ -65,9 +65,9 @@ const DashboardLayout = () => {
   };
 
   return (
-    <div className="flex h-screen bg-white font-sans text-gray-800">
+    <div className="flex h-screen bg-white font-sans text-gray-800 hide-scrollbar">
       {/* Sidebar */}
-      <div className="w-64 shadow-2xl flex flex-col border-r border-gray-200" style={{ backgroundColor: '#be741e' }}>
+      <div className="w-64 shadow-2xl flex flex-col border-r border-gray-200 hide-scrollbar" style={{ backgroundColor: '#be741e' }}>
         {/* Logo */}
         <div className="p-6 border-b border-gray-200 flex items-center">
           <img src={logo} alt="TradeWise logo" className='w-[50px] h-[40px] rounded-full mr-1' />
@@ -75,7 +75,7 @@ const DashboardLayout = () => {
         </div>
       
         {/* Navigation */}
-        <nav className="flex-grow p-6 space-y-2">
+        <nav className="flex-grow p-6 space-y-2 hide-scrollbar">
           <ul>
             {tabs.map((tab) => (
               <li key={tab.id} className="mt-1">
@@ -105,7 +105,7 @@ const DashboardLayout = () => {
       </div>
 
       {/* Main content area */}
-      <div className="flex-1 flex flex-col overflow-hidden bg-white text-gray-800">
+      <div className="flex-1 flex flex-col overflow-hidden bg-white text-gray-800 hide-scrollbar">
         {/* Header */}
         <header className="flex items-center justify-between p-6 bg-white border-b border-gray-200 shadow-sm text-gray-800">
           <div className="text-3xl font-semibold">Welcome Back, User!</div>
@@ -125,7 +125,7 @@ const DashboardLayout = () => {
         </header>
 
         {/* Main content */}
-        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
+        <main className="flex-1 overflow-x-hidden hide-scrollbar overflow-y-auto bg-gray-50 p-6">
           {renderContent()}
         </main>
       </div>
