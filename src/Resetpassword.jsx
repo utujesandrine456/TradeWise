6de59bc573e8c8e268bbb8ea7309dc8de09daa72
utujesandrine456 <utujesandrine456@gmail.com>
2025-09-10 11:43:17 +0200

    Add Resetpassword component

---
+++ src/Resetpassword.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Signupimage from './assets/Login.jpg'
+
+
+
+const Resetpassword = () => {
+  return (
+    <div className='bg-black h-screen flex flex-1 justify-between'>
+      <div className='w-1/2 h-[100vh] flex items-center justify-center bg-gray-50'>
+        <img src={Signupimage} alt="login" className='w-full h-full object-cover' />
+      </div>
+      
+      <div className='flex flex-col bg-white pt-10'>
+        <h1 className='text-black text-5xl font-bold my-4 text-center mb-10'>Welcome Back at <span className='text-[#BE741E]'>TradeWise</span></h1>
+        <p className='text-black/80 text-normal text-center px-auto mb-4'>Please provide a valid email address so we can send you <br></br>  a code or link to reset your password.</p>
+        <div className='flex flex-col space-y-4 mx-10 my-4'>
+          <input type="text" placeholder="Current Password" className='w-100 py-2 px-4 border text-sm border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#BE741E] focus:border-transparent' />
+          <input type="text" placeholder="New Password" className='w-100 py-2 px-4 border text-sm border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#BE741E] focus:border-transparent' />
+          <input type="text" placeholder="Confirm Password" className='w-100 py-2 px-4 border text-sm border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#BE741E] focus:border-transparent' />
+        </div>
+        <button type="submit" className="flex w-40 mx-auto text-center py-2 px-6 mt-4 bg-[#BE741E] text-sm text-white font-medium rounded-lg hover:bg-[#cc8b3a]" >Reset Password</button>
+      </div>
+    </div>
+  )
+}
+
+export default Resetpassword;
